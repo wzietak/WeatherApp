8c941e3c6e7873c96b0ecbf19925ca7a72090cad
Wiktoria Zietak <maullido.blog@gmail.com>
2025-05-16 16:31:09 +0200

    Front apki - początek

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+from api.geocoding_api import GeocodingAPI
+from api.weather_api import WeatherAPI
+
+st.title("Weather App")
+
+with st.form('location_form'):
+    st.write("Search location by city and country:")
+    city_input = st.text_input("City")
+    country_input = st.text_input("Country")
+    search_button = st.form_submit_button("Search")
+
+coding = GeocodingAPI()
+weather = WeatherAPI()
+
+if search_button:
+    country_coded = coding.convertCountryToISOCode(city_input)
+    location = coding.getLocation(city_input, country_coded)
+    st.write(location)
