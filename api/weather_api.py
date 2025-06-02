47b83135e2c2dfe411853e60513c1e9940a2aa7c
Wiktoria Zietak <maullido.blog@gmail.com>
2025-06-02 15:10:03 +0200

    Usunięcie nieużywanego URL i metody do pobierania danych historycznych. Dodanie komentarzy dokumentacyjnych

--- api/weather_api.py
+++ api/weather_api.py
@@ -2,19 +2,28 @@ import requests
 import streamlit as st
 
 class WeatherAPI:
+    """
+    Klasa do pobierania aktualnych danych pogodowych z API OpenWeatherMap.
+    """
     def __init__(self):
+        """
+        Inicjalizuje obiekt klasy WeatherAPI. Pobiera klucz API z konfiguracji Streamlit i ustawia podstawowy URL dla zapytań pogodowych.
+        """
         self.API_KEY = st.secrets["api"]["key"]
         self.url = "https://api.openweathermap.org/data/2.5/weather?lat="
-        self.url_historic = "https://api.openweathermap.org/data/3.0/onecall/timemachine?lat="
 
     def getWeatherForLocation(self, lat, lon):
-        request = f"{self.url}{lat}&lon={lon}&units=metric&appid={self.API_KEY}"
-        response = requests.get(request)
-        data = response.json()
-        return data
+        """
+        Pobiera aktualne dane pogodowe dla podanej lokalizacji.
 
-    def getHistoricWeatherForLocation(self, lat, lon, timestamp):
-        request = f"{self.url_historic}{lat}&lon={lon}&dt={timestamp}&appid={self.api_key}"
+        Args:
+            lat (float): szerokość geograficzna
+            lon (float): długość geograficzna
+
+        Returns:
+            data (dict): dane pogodowe w formacie JSON z OpenWeatherMap API
+        """
+        request = f"{self.url}{lat}&lon={lon}&units=metric&appid={self.API_KEY}"
         response = requests.get(request)
         data = response.json()
         return data
